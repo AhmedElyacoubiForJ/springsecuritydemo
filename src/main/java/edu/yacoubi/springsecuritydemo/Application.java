927208dc2167a3fda622a01e6ba0ffce85500862
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-30 19:52:48 +0200

    log tests, refact

--- src/main/java/edu/yacoubi/springsecuritydemo/Application.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/Application.java
@@ -2,26 +2,22 @@ package edu.yacoubi.springsecuritydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@Configuration
-//@EnableWebSecurity(debug = true)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+}
 
-    @RestController
-    class BasicController {
-        @GetMapping("/hello")
-        public ResponseEntity<String> get() {
-            return ResponseEntity.ok("Hello");
-        }
+@RestController
+class BasicController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello");
     }
 }

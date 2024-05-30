2bde333b5f8bfab6447c8da9f9fa45d721a7fddf
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-30 19:38:32 +0200

    before to switch to previews branch

--- src/main/java/edu/yacoubi/springsecuritydemo/Application.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/Application.java
@@ -12,15 +12,31 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
@@ -29,6 +45,7 @@ public class Application {
 @RequiredArgsConstructor
 class BasicController {
     private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/hello")
     public ResponseEntity<String> get() {
@@ -44,7 +61,8 @@ class BasicController {
                 .unauthenticated(loginDTO.getUsername(), loginDTO.getPassword());
 
         authenticationManager.authenticate(unAuthenticated);
-        return ResponseEntity.ok("Login");
+        String jwtToken = jwtUtil.generate(loginDTO.getUsername());
+        return ResponseEntity.ok(jwtToken);
     }
 }
 
@@ -63,5 +81,22 @@ class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+}
+@Service
+@RequiredArgsConstructor
+class UserDetailsServiceImpl implements UserDetailsService {
+    private final BCryptPasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // user from db
+        Map<String, String> map = new HashMap<>();
+        map.put("leo", encoder.encode("123"));
+
+        if (map.containsKey(username)) {
+            return new User(username, map.get(username), new ArrayList<GrantedAuthority>());
+        }
+        throw new UsernameNotFoundException("User " + username + " not found");
     }
 }

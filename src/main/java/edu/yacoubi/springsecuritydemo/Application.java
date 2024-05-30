041b72b86c5f59d7b778da5ffe75a275cc0a75a4
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-30 11:23:39 +0200

    initial commit

---
+++ src/main/java/edu/yacoubi/springsecuritydemo/Application.java
@@ -0,0 +1,23 @@
+package edu.yacoubi.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RestController
+    class BasicController {
+        @GetMapping("/hello")
+        public ResponseEntity<String> get() {
+            return ResponseEntity.ok("Hello");
+        }
+    }
+}

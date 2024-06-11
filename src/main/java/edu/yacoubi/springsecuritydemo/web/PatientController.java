8b7aca6fdf085374e034e06359a39312d3605630
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-11 15:44:03 +0200

    patients pagination view

--- src/main/java/edu/yacoubi/springsecuritydemo/web/PatientController.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/web/PatientController.java
@@ -3,6 +3,8 @@ package edu.yacoubi.springsecuritydemo.web;
 import edu.yacoubi.springsecuritydemo.entites.Patient;
 import edu.yacoubi.springsecuritydemo.repository.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,8 @@ public class PatientController {
 
     @GetMapping("/index")
     public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+        Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(0, 4));
+        model.addAttribute("patientsPage", patientsPage.getContent());
         return "patients";
     }
 }

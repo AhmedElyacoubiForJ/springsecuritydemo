d332765c3db94b5b51c377f0b74bc931e4028542
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-11 02:58:17 +0200

    inInMemoryUserDetailsManager test Demo

--- src/main/java/edu/yacoubi/springsecuritydemo/Application.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/Application.java
@@ -1,8 +1,10 @@
 package edu.yacoubi.springsecuritydemo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 

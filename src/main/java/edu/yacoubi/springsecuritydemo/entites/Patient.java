7117b716c55fe7345c926c7846ab4f9f7af9e153
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-12 16:40:18 +0200

    basic project is done, time to move to secure our application

--- src/main/java/edu/yacoubi/springsecuritydemo/entites/Patient.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/entites/Patient.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -20,18 +21,10 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 public class Patient {
     @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    @NotEmpty @Size(min = 4, max =20)
+    @NotBlank @Size(min = 4, max =20)
     private String name;
     private LocalDate birthday;
     private boolean isSick;
     @Min(10)
     private int score;
 }
-
-// Bean command runner
-   /* @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            System.out.println("Hello World");
-        };
-    }*/

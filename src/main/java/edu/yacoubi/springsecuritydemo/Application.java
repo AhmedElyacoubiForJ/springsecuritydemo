b3a3a1bc3b64e84de54d08c46814a3cba471b562
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-13 15:58:10 +0200

    create users using JdbcUserDetailsManager & commandRunner, cool

--- src/main/java/edu/yacoubi/springsecuritydemo/Application.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/Application.java
@@ -7,31 +7,47 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Date;
 
 @SpringBootApplication
-@AllArgsConstructor
 public class Application {
-    private final PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
+    //@Bean
+    public CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
         return args -> {
-           /* patientRepository.save( new Patient(null, "Steve McQueen", LocalDate.of(1930, Month.MARCH, 24), false, 26) );
+            patientRepository.save( new Patient(null, "Steve McQueen", LocalDate.of(1930, Month.MARCH, 24), false, 26) );
             patientRepository.save( new Patient(null, "Jesse James", LocalDate.of(1847, Month.SEPTEMBER, 5), false, 50) );
             patientRepository.save( new Patient(null, "John Wayne", LocalDate.of(1907, Month.JUNE, 26), true, 54) );
             patientRepository.save( new Patient(null, "Robert Mitchum", LocalDate.of(1917, Month.AUGUST, 17), false, 101) );
             patientRepository.save( new Patient(null, "Paul Newman", LocalDate.of(1917, Month.JANUARY, 26), false, 101) );
-            patientRepository.save( new Patient(null, "Audie Murphy", LocalDate.of(1925, Month.JUNE, 20), false, 343) );*/
+            patientRepository.save( new Patient(null, "Audie Murphy", LocalDate.of(1925, Month.JUNE, 20), false, 343) );
+        };
+    }
 
-//            patientRepository.findAll().forEach(System.out::println);
+    @Bean
+    CommandLineRunner createUsers(
+            JdbcUserDetailsManager jdbcUserDetailsManager,
+            PasswordEncoder encoder) {
+        return args -> {
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user1").password(encoder.encode("123")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user2").password(encoder.encode("123")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("admin").password(encoder.encode("123")).roles("USER", "ADMIN").build()
+            );
         };
     }
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
---test only
-INSERT INTO users (username, password, enabled) VALUES ('user1', '1234', true);

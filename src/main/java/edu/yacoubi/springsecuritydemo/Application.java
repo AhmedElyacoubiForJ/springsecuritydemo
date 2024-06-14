fefe35487ae55a254bc1169d0030430c1a9c4ddd
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-14 02:35:19 +0200

    creates users w. custom service, AccountServcie

--- src/main/java/edu/yacoubi/springsecuritydemo/Application.java
+++ src/main/java/edu/yacoubi/springsecuritydemo/Application.java
@@ -2,7 +2,9 @@ package edu.yacoubi.springsecuritydemo;
 
 import edu.yacoubi.springsecuritydemo.entites.Patient;
 import edu.yacoubi.springsecuritydemo.repository.PatientRepository;
-import lombok.AllArgsConstructor;
+import edu.yacoubi.springsecuritydemo.security.entities.AppRole;
+import edu.yacoubi.springsecuritydemo.security.entities.AppUser;
+import edu.yacoubi.springsecuritydemo.security.service.AccountService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,6 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Date;
 
 @SpringBootApplication
 public class Application {
@@ -62,6 +63,36 @@ public class Application {
                 jdbcUserDetailsManager.createUser(
                         User.withUsername("admin").password(encoder.encode("123")).roles("USER", "ADMIN").build()
                 );
+            }
+        };
+    }
+
+    @Bean
+    CommandLineRunner accountServiceCreateUsers(AccountService accountService) {
+        return args -> {
+//            accountService.addNewRole("USER");
+//            accountService.addNewRole("ADMIN");
+
+
+            AppUser appUser;
+
+            appUser = accountService.loadUserByUsername("user11");
+            if (appUser == null) {
+                accountService.addNewUser("user11", "123", "user11@gmail.com", "123");
+                accountService.addRoleToUser("user11", "USER");
+            }
+
+            appUser = accountService.loadUserByUsername("user22");
+            if (appUser == null) {
+                accountService.addNewUser("user22", "123", "user22@gmail.com", "123");
+                accountService.addRoleToUser("user22", "USER");
+            }
+
+            appUser = accountService.loadUserByUsername("admin33");
+            if (appUser == null) {
+                accountService.addNewUser("admin33", "123", "user33@gmail.com", "123");
+                accountService.addRoleToUser("admin33", "USER");
+                accountService.addRoleToUser("admin33", "ADMIN");
             }
         };
     }
